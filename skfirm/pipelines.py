34ebd64262800c75e4b655642938ec48cf61002c
Akihiro Kawanishi <clp14044@stn.nitech.ac.jp>
2021-05-25 19:47:05 +0900

    spiderのテストとパイプラインの追加

--- skfirm/pipelines.py
+++ skfirm/pipelines.py
@@ -55,17 +55,50 @@ class FirmwarePipeline(FilesPipeline):
             self.store.persist_file(path, buf, info)
         return checksum
 
+   # overrides function from FilesPipeline
+    def get_media_requests(self, item, info):
+        # check for mandatory fields
+        for x in ["vendor", "url"]:
+            if x not in item:
+                raise DropItem(
+                    "Missing required field '%s' for item: " % x)
+
+        # resolve dynamic redirects in urls
+        for x in ["mib", "sdk", "url"]:
+            if x in item:
+                split = urllib.parse.urlsplit(item[x])
+                # remove username/password if only one provided
+                if split.username or split.password and not (split.username and split.password):
+                    item[x] = urllib.parse.urlunsplit(
+                        (split[0], split[1][split[1].find("@") + 1:], split[2], split[3], split[4]))
+
+                if split.scheme == "http":
+                    item[x] = urllib.request.urlopen(item[x]).geturl()
+
+        # check for filtered url types in path
+        url = urllib.parse.urlparse(item["url"])
+        if any(url.path.endswith(x) for x in [".pdf", ".php", ".txt", ".doc", ".rtf", ".docx", ".htm", ".html", ".md5", ".sha1", ".torrent"]):
+            raise DropItem("Filtered path extension: %s" % url.path)
+        elif any(x in url.path for x in ["driver", "utility", "install", "wizard", "gpl", "login"]):
+            raise DropItem("Filtered path type: %s" % url.path)
+
+        # generate list of url's to download
+        item[self.files_urls_field] = [item[x]
+                                       for x in ["mib", "url"] if x in item]
+
+        # pass vendor so we can generate the correct file path and name
+        return [Request(x, meta={"ftp_user": "anonymous", "ftp_password": "chrome@example.com", "vendor": item["vendor"]}) for x in item[self.files_urls_field]]
+
     # overrides function from FilesPipeline
     def item_completed(self, results, item, info):
         item['files'] = []
         if isinstance(item, dict) or files in item.fields:
             item['files'] = [x for ok, x in results if ok]
 
-"""
-mongoDB 4.0以降じゃないとトランザクション処理ができません！！！！
-なのでとりあえずエラー処理は後回し
-"""
-        #s = client.start_session()
+        """
+        mongoDB 4.0以降じゃないとトランザクション処理ができません！！！！
+        なのでとりあえずエラー処理は後回し
+        """
         if self.client:
             try:
                 copy = item.deepcopy()
